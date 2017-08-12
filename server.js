00176207dcb42a4ea6caa432939a72981874a45a
Nikr94 <nrg051194@gmail.com>
2017-08-12 18:44:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res){
-    res.sendFile(createTemplate(articleone));
+    res.send(createTemplate(articleone));
 });
 
 app.get('/article-two', function (req, res){
