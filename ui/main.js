29afe30a305466e6e2158fdf673839cd8a40f5fe
Nikr94 <nrg051194@gmail.com>
2017-08-12 19:46:06 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,7 +5,7 @@ element.innerHTML ='New value';
 
 //move th eimage
 
-var img =document.getElementById('img');
+var img =document.getElementById('madi');
 img.onclick = function () {
     img.style.marginLeft ='100px';
 };
