43377bc493f39e8dbd677b0d2f43caaa22b4c52c
Nikr94 <nrg051194@gmail.com>
2017-08-12 22:02:05 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -62,6 +62,11 @@ var htmltemplate= `
 return htmltemplate;
 }
 
+var counter =0;
+app.get("/counter", function (req, res) {
+  counter = counter + 1;
+  res.send(counter.toString());
+});
 
 
 app.get('/', function (req, res) {
