4f5338621336916c18c081aeeef0bb605f74c591
Nikr94 <nrg051194@gmail.com>
2017-08-12 18:35:34 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,41 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleone ={
+    title:'Article one template',
+    heading:'Article one',
+    date:'sep 05,2016',
+    content:` <p>
+                In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.In this we are going to learn about server side js.</p>
+        
+        <p>server side js.server side js.server side js.server side js.server side js.server side js.v</p>
+        <p>server side js.server side js.server side js.server side js.server side js.server side js.server side js.server side js.server side js.server side js.
+        </p>`
+    
+};
+var htmltemplate=
+   <!DOCTYPE html>
+<html>
+    <head>
+        <title>${title}</title>
+        <meta name="viewport" content="width-device-width, initial-scale-1"/>
+       <link href="/ui/style.css" rel="stylesheet">
+    </head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <h1>${heading}</h1>
+        <div>${date}</div>
+        ${content}
+        </div>
+    </body>
+</html>
+    
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
