55d7e4fb4f970d9119ebb66ff3a43d8c10b3c54e
Nikr94 <nrg051194@gmail.com>
2017-08-12 22:16:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -17,9 +17,11 @@ img.onclick = function () {
 };*/
 var button = document.getElementById("counter");
 var counter =0;
+button.onclick = function(){
 
 
 
 counter = counter + 1;
 var span = document.getElementById("count");
 span.innerHTML =counter.toString();
+};
