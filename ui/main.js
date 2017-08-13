a18dd2383c0309da4238aacffe504df87811a955
Nikr94 <nrg051194@gmail.com>
2017-08-13 14:06:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -15,13 +15,27 @@ function moveRight()
 img.onclick = function () {
     var interval = setInterval(moveRight,100);
 };*/
+
 var button = document.getElementById("counter");
 var counter =0;
 button.onclick = function(){
 
+var request =new  XMLHttpRequest();
+
+request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        
+        if(request.status === 200){
+         var counter = request.responseText;   
+         var span = document.getElementById("count");
+         span.innerHTML =counter.toString();
+        }
+    }
+    
+};
+
+request.open('GET', "http://nrg051194.imad.hasura-app.io/counter",true);
+request.send(null);
 
 
-counter = counter + 1;
-var span = document.getElementById("count");
-span.innerHTML =counter.toString();
 };
