2998611280b81bcf57f91dd45078c5497c82ce24
Nikr94 <nrg051194@gmail.com>
2017-08-14 15:42:12 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -50,8 +50,7 @@ for( var i=0;i<names.length;i++){
 }
 var ul = document.getElementById("namelist");
 ul.innerHTML =list;*/
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var request =new  XMLHttpRequest();
 
 request.onreadystatechange = function(){
@@ -73,7 +72,8 @@ ul.innerHTML =list;
     }
     
 };
-
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
 request.open('GET', "http://nrg051194.imad.hasura-app.io/submit-name?name=" + name,true);
 request.send(null);
 
