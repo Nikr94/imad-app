f986c2bb7ddf21586cd8e4c9d28c56e05bc8801c
Nikr94 <nrg051194@gmail.com>
2017-08-14 15:41:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -40,8 +40,7 @@ request.send(null);
 
 };
 
-var nameInput = document.getElementById("name");
-var name = nameInput.value;
+
 var submit = document.getElementById("submit_btn");
 submit.onclick= function(){
 /*var names =['name1','name2','name3'];
@@ -51,6 +50,8 @@ for( var i=0;i<names.length;i++){
 }
 var ul = document.getElementById("namelist");
 ul.innerHTML =list;*/
+var nameInput = document.getElementById("name");
+var name = nameInput.value;
 var request =new  XMLHttpRequest();
 
 request.onreadystatechange = function(){
