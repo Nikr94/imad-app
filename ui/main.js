ac6220f39716d1c63f1af6a8b0a6bf3fb1c6eb75
Nikr94 <nrg051194@gmail.com>
2017-08-14 15:31:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -26,7 +26,7 @@ request.onreadystatechange = function(){
     if(request.readyState === XMLHttpRequest.DONE){
         
         if(request.status === 200){
-         var counter = request.responseText;   
+        var counter = request.responseText;   
          var span = document.getElementById("count");
          span.innerHTML =counter.toString();
         }
@@ -44,11 +44,36 @@ var nameInput = document.getElementById("name");
 var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick= function(){
-var names =['name1','name2','name3'];
+/*var names =['name1','name2','name3'];
 var list ='';
 for( var i=0;i<names.length;i++){
   list += '<li>' + names[i] + '</li>';
 }
 var ul = document.getElementById("namelist");
+ul.innerHTML =list;*/
+var request =new  XMLHttpRequest();
+
+request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        
+        if(request.status === 200){
+        /* var counter = request.responseText;   
+         var span = document.getElementById("count");
+         span.innerHTML =counter.toString();*/
+         var names =['name1','name2','name3'];
+         var list ='';
+         for( var i=0;i<names.length;i++){
+         list += '<li>' + names[i] + '</li>';
+}
+var ul = document.getElementById("namelist");
 ul.innerHTML =list;
+        }
+    }
+    
+};
+
+request.open('GET', "http://nrg051194.imad.hasura-app.io/submit-name?name=",true);
+request.send(null);
+
+
 };
