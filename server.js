84abbcba968201777760a9c6b8cb09190cf588f2
Nikr94 <nrg051194@gmail.com>
2017-08-19 18:56:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -112,8 +112,20 @@ app.get('/submit-name',function(req, res){
     
 });
 app.get('/:articleName', function (req, res){
-   var  articleName = req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+  // var  articleName = req.params.articleName;
+  //  res.send(createTemplate(articles[articleName]));
+    pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function(err,result){
+    if(err){
+         res.status(500).send(err, toString());
+            }else{
+            if(result.rows.length === 0){
+                res.status(404).send("Article not found");
+            }else{
+                var articleData = rows[0];
+                res.send(createTemplate(articleData));
+          }
+            }
+            }); 
 });
 
 
