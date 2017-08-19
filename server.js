bad5859be6bf62694ce9ebbd8e47bd11ee3e007b
Nikr94 <nrg051194@gmail.com>
2017-08-19 19:10:01 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,7 +81,7 @@ var pool = new Pool(config);
 app.get('/test-db',function (req, res){
     pool.query('SELECT * FROM test', function(err, result){
         if(err){
-            res.status(500).send(err, toString());
+            res.status(500).send(err.toString());
             }else{
                 res.send(JSON.stringify(result.rows));
             }
@@ -116,7 +116,7 @@ app.get('/articles/articleName', function (req, res){
   //  res.send(createTemplate(articles[articleName]));
     pool.query("SELECT * FROM article WHERE title = $1",[req.params.articleName],function(err,result){
     if(err){
-         res.status(500).send(err, toString());
+         res.status(500).send(err.toString());
             }else{
             if(result.rows.length === 0){
                 res.status(404).send("Article not found");
